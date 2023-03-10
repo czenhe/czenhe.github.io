74c978ec17fa7d499d21df5a48f7b026f00dec01
EH, C <czenhe@gmail.com>
2023-03-10 19:20:55 +0900

    modify svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -4,23 +4,24 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		alias: {
-			$components: path.resolve('./src/lib/components'),
-			$layouts: path.resolve('./src/lib/layouts'),
-			$utils: path.resolve('./src/lib/utils'),
-			$styles: path.resolve('./src/lib/styles'),
-			$stores: path.resolve('./src/lib/stores'),
-			$types: path.resolve('./src/lib/types'),
-			$images: path.resolve('./src/lib/images')
-		}
-	}
+    alias: {
+      $components: path.resolve('./src/lib/components'),
+      $layouts: path.resolve('./src/lib/layouts'),
+      $utils: path.resolve('./src/lib/utils'),
+      $styles: path.resolve('./src/lib/styles'),
+      $stores: path.resolve('./src/lib/stores'),
+      $services: path.resolve('./src/lib/services'),
+      $types: path.resolve('./src/lib/types'),
+      $images: path.resolve('./src/lib/images')
+    }
+  }
 };
 
 export default config;
