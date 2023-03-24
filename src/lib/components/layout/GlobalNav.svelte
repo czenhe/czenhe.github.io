c4de5ce7e0e3eae7775f1702b81617fac0046780
EH, C <czenhe@gmail.com>
2023-03-25 01:38:25 +0900

    네비게이션 수정

--- src/lib/components/layout/GlobalNav.svelte
+++ src/lib/components/layout/GlobalNav.svelte
@@ -25,8 +25,8 @@
     <li class="navigation-item" aria-current={$page.url.pathname === '/essay' ? 'page' : undefined}>
       <a href="/essay">Essay</a>
     </li>
-    <li class="navigation-item" aria-current={$page.url.pathname === '/study' ? 'page' : undefined}>
-      <a href="/study">Study</a>
+    <li class="navigation-item">
+      <a href="https://devlog.czenhe.me/" target="_blank" rel="noreferrer">Study</a>
     </li>
     <li class="navigation-item">
       <a href="https://theafterhours.page/" target="_blank" rel="noreferrer">Project</a>
