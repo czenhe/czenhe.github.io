0563a157659d191c0f2629cb316ed3c65c2569bf
EH, C <czenhe@gmail.com>
2023-05-15 23:49:14 +0900

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,36 @@
 <script>
+  import { onMount } from 'svelte';
 </script>
 
 <svelte:head>
-  <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
+  <title>Czenhe + me</title>
+  <meta name="description" content="Czenhe's portfolio site" />
 </svelte:head>
+<div class="contents" id="fullpage">
+  <div class="section section01">
+    <div id="intro" />
+    <div class="fullBackground" />
+    <div class="scroll-btn">
+      <a class="flex column" href="#news" data-menuanchor="news">
+        <span>SCROLL</span>
+        <span class="material-symbols-outlined"> keyboard_double_arrow_down </span>
+      </a>
+    </div>
+  </div>
+  <div class="section section02">section2</div>
+</div>
 
-<style>
+<style lang="scss">
+  .material-symbols-outlined {
+    font-size: 1rem;
+  }
+  .contents {
+    position: relative;
+    top: 0px;
+    height: 100%;
+    touch-action: none;
+  }
+  .section {
+    position: relative;
+  }
 </style>
