8e215e9768850f0f388c897ec490e9190172f75a
EH, C <czenhe@gmail.com>
2023-07-13 00:22:38 +0900

    setting style system

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['./src/**/*.{html,js,vue,ts}'],
+    options: {},
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
